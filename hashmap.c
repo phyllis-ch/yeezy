ee5c16d713c25c177109e70a816fed9878df542d
phyllis-ch <phylliscpx0403@proton.me>
2026-05-08 04:31:23 +0800

    hashmap push now takes a bucket size

--- hashmap.c
+++ hashmap.c
@@ -2,19 +2,19 @@
 
 unsigned long hash(const char *str)
 {
-   unsigned long hash = 1469598103934665603;
+   unsigned long hash = 1469598103934665603; /* constant I took from wikipedia*/
 
    while (*str) {
       hash ^= (unsigned char)(*str++);
-      hash *= 1099511628211;
+      hash *= 1099511628211; /* constant I took from wikipedia*/
    }
 
    return hash;
 }
 
-void hm_push(Node *map, unsigned long hash_result, Entry *entry)
+void hm_push(Node *map, unsigned long hash_result, int bucket_size, Entry *entry)
 {
-   int idx = hash_result % 1024;
+   int idx = hash_result % bucket_size;
    map[idx].hash = hash_result;
    map[idx].ptr = entry;
 }
