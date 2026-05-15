bb003cd4be6642cd9b0def1002f2d26846a7cf56
phyllis-ch <phylliscpx0403@proton.me>
2026-05-15 18:16:38 +0800

    Nothing important

--- fuzzy.c
+++ fuzzy.c
@@ -9,7 +9,7 @@ char *get_basename(char *text)
    return s;
 }
 
-// check for seperators in a base filename e.g. my_file.py
+/* check for seperators in a base filename e.g. my_file.py */
 int match_seperator(char c)
 {
    return c == '/' || c == '_' || c == '-' || c == ' ' || c == '.';
@@ -18,18 +18,18 @@ int match_seperator(char c)
 int get_basescore(const char *pattern, char *text)
 {
    int score = 0;
-   int pi = 0; // pattern index
+   int pi = 0; /* pattern index */
    int consecutive_matches = 0;
    int first_match = -1;
    int len = strlen(text);
 
    for (int ti = 0; text[ti] != '\0'; ++ti) {
-      if (pattern[pi] == '\0') break; // premature exit
+      if (pattern[pi] == '\0') break; /* premature exit */
 
       if (tolower(text[ti]) == tolower(pattern[pi])) {
-         if (first_match == -1) first_match = ti; // record text index of first match
+         if (first_match == -1) first_match = ti; /* record text index of first match */
 
-         score += 10; // match
+         score += 10; /* match */
          score -= len;
          if (consecutive_matches > 0) score += 15;
          if (ti == 0 || match_seperator(text[ti - 1])) score += 20;
@@ -42,8 +42,8 @@ int get_basescore(const char *pattern, char *text)
       }
    }
 
-   if (pattern[pi]) return -65536;  // No complete match for entire pattern
-   if (first_match > 0) score -= first_match;  // Late first match
+   if (pattern[pi]) return -65536;  /* No complete match for entire pattern */
+   if (first_match > 0) score -= first_match;  /* Late first match */
 
    return score;
 }
